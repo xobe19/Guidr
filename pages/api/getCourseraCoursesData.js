09b238e6f45cd7e0b5c2bf16c556f4412f6696d2
Hitesh Lalwani <lwni@protonmail.com>
2022-11-01 22:54:39 +0530

    added APIs for udemy,coursera and codecademy course data

---
+++ pages/api/getCourseraCoursesData.js
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import clientPromise from "../../lib/mongo_connection";
+export default async function getUdemyCoursesData(
+  { query: { jobName, limit } },
+  res
+) {
+  const client = await clientPromise;
+  const db = client.db("guidr");
+  const collection = db.collection("coursera");
+  const result = await collection
+    .find({ $text: { $search: jobName } })
+    .limit(limit * 1)
+    .toArray();
+  res.json(result);
+}
