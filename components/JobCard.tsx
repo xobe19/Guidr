baa1d21539d257b4e28cedb3ceba91dd85ee7248
vikasrushi <0xvikas@gmail.com>
2022-11-02 21:26:24 +0530

    jobcard updated added

--- components/JobCard.tsx
+++ components/JobCard.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import Link from "next/link";
 const JobCard = () => {
   return (
     <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
@@ -15,7 +17,10 @@ const JobCard = () => {
         <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
           Job Description with avg salary:
         </p>
-        <ul className="flex space-x-4 sm:mt-0">
+
+        {/* all svg logos removed replacing with button can use this component later */}
+
+        {/* <ul className="flex space-x-4 sm:mt-0">
           <li>
             <a
               href="#"
@@ -72,7 +77,27 @@ const JobCard = () => {
               </svg>
             </a>
           </li>
-        </ul>
+        </ul> */}
+
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <Link href="/courses">See Courses</Link>
+          <svg
+            aria-hidden="true"
+            className="ml-2 -mr-1 w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
       </div>
     </div>
   );
