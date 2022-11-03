195fc63ccf84c90e71968c5d55f0b1357cb49588
vikasrushi <0xvikas@gmail.com>
2022-11-03 18:59:19 +0530

    warnings fixed

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -12,9 +12,9 @@ const CourseCard = (props) => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           ></path>
           <path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path>
         </svg>
